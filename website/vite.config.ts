53e901ccbf2b74de29c8e1f09ced94711e023c99
GuiEpi <guillaume.coussot@epitech.eu>
2025-01-04 22:32:11 +0100

    🐳 Update docker config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,6 +5,10 @@ import mdx from '@mdx-js/rollup';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 8081,
+  },
   plugins: [
     {enforce: 'pre', ...mdx()},
     react()
