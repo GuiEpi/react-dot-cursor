272389712a6e8abb58d91ccc0b85ffcaacc389cd
GuiEpi <guillaume.coussot@epitech.eu>
2025-01-06 00:07:01 +0100

    💄 Fix flex-grow when no code

--- website/src/components/step-card.tsx
+++ website/src/components/step-card.tsx
@@ -7,7 +7,7 @@ export const StepCard: React.FC<{ step: number; description: string; code?: stri
     <div className="relative flex items-center justify-center w-10 h-10 text-xl font-semibold tracking-tight mb-2 bg-rose-500 rounded-full">
       <span className="relative text-white">{step}</span>
     </div>
-    <p className="text-sm text-muted-foreground mb-2 flex-grow">{description}</p>
+    <p className={`text-sm text-muted-foreground mb-2 ${code ? 'flex-grow' : ''}`}>{description}</p>
     {code && (
       <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold flex-grow">
         {code}
