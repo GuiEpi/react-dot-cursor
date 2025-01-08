374b106b64c3c9f28c5088d44e3a0f261d5f91a3
GuiEpi <guillaume.coussot@epitech.eu>
2025-01-08 03:13:08 +0100

    💄 Add syntax highlighting thanks to rehype

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import mdx from '@mdx-js/rollup';
+import rehypeHighlight from 'rehype-highlight';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,15 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 8081,
   },
-  plugins: [{ enforce: 'pre', ...mdx() }, react()],
+  plugins: [
+    {
+      enforce: 'pre',
+      ...mdx({
+        rehypePlugins: [rehypeHighlight],
+      }),
+    },
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
