b2ad783853f4833df5af7fe163180d09f39c9899
GuiEpi <guillaume.coussot@epitech.eu>
2025-01-09 18:46:31 +0100

    ✏️ Fix storageKey

--- website/src/providers/theme-provider.tsx
+++ website/src/providers/theme-provider.tsx
@@ -23,7 +23,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 export function ThemeProvider({
   children,
   defaultTheme = 'system',
-  storageKey = 'vite-ui-theme',
+  storageKey = 'react-dot-cursor-ui-theme',
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(() => (localStorage.getItem(storageKey) as Theme) || defaultTheme);
