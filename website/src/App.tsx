39e07af701c61bc5e2d656977dfd325df640f939
GuiEpi <guillaume.coussot@epitech.eu>
2025-08-04 09:32:34 +0200

    🔖 Release v1.0.0

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -19,7 +19,7 @@ const theme: CursorTheme = {
         height: 'auto',
       },
       content: {
-        text: 'v0.2.4',
+        text: 'v1.0.0',
         style: {
           fontSize: '12px',
           fontWeight: 'bold',
